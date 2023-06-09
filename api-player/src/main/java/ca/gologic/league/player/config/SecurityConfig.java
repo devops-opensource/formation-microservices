74c3b33d4265004e7ab1e06a5017959a194afec8
Haidar Dahnoun <haidar.dahnoun@gologic.ca>
2023-06-09 10:41:25 -0400

    Remove security on player endpoint

--- api-player/src/main/java/ca/gologic/league/player/config/SecurityConfig.java
+++ api-player/src/main/java/ca/gologic/league/player/config/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
       new AntPathRequestMatcher("/swagger-ui/**"),
       new AntPathRequestMatcher("/swagger-ui.html"),
       new AntPathRequestMatcher("/swagger-resources/**"),
-      new AntPathRequestMatcher("/v3/api-docs/**")
+      new AntPathRequestMatcher("/v3/api-docs/**"),
+      new AntPathRequestMatcher("/api/v1/player")
   );
 
   private static final RequestMatcher PROTECTED_URLS = new NegatedRequestMatcher(PUBLIC_URLS);
